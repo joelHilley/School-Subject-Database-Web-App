ffdc73ee4974546a81f9e4e8d45fffc2799de583
joelHilley <66706087+joelHilley@users.noreply.github.com>
2020-12-14 05:55:02 +1300

    fixed bug in app.set

--- Web App/server.js
+++ Web App/server.js
@@ -19,12 +19,11 @@ MongoClient.connect('mongodb+srv://admin:newpass@cluster0.vxttk.mongodb.net/test
         app.use(bodyParser.urlencoded({ extended: true }))
 
         //routes
-        /*app.get('/', (req, res) => {
-            res.sendFile('/Users/joelh/Desktop/IT6037 Data Acess and Management/IT6037Project_92060012/School-Subject-Database-Web-App/Web App/' + 'index.html')
-        })*/
-
         app.get('/', (req, res) => {
             res.sendFile('/Users/joelh/Desktop/IT6037 Data Acess and Management/IT6037Project_92060012/School-Subject-Database-Web-App/Web App/' + 'index.html')
+        })
+
+        app.get('/', (req, res) => {
             db.collection('subjects').find().toArray()
                 .then(subjects => {
                     res.render('index.ejs', { subjects: subjects})
